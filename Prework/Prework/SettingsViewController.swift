77a2b5ba8554c0e0893fa4837a0047962ffbaccc
jguti24 <jguti243@fiu.edu>
2022-08-25 22:45:21 -0400

    Added prework files to repository

---
+++ Prework/Prework/SettingsViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SettingsViewController.swift
+//  Prework
+//
+//  Created by Jarol Gutierrez on 8/25/22.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.title = "Tip Calculator"
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("view will appear")
+        // This is a good place to retrieve the default tip percentage from UserDefaults
+        // and use it to update the tip amount
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did appear")
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("view will disappear")
+    }
+
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("view did disappear")
+    }
+
+}
